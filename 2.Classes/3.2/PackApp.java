372d20852948f0a99b3006e9558731f2b223515b
Thiago Pereira <86577632+thpereira1@users.noreply.github.com>
2021-08-28 03:00:20 -0300

    Create PackApp.java

---
+++ 2.Classes/3.2/PackApp.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import Figures.*;
+
+class PackApp {
+    public static void main (String[] args) {
+        PackFrame frame = new PackFrame();
+        frame.setVisible(true);
+    }
+}
+
+class PackFrame extends JFrame {
+    Rect r1;
+    Ellipse e1;
+    Triangulo t1;
+
+    PackFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Java Packages");
+        this.setSize(350, 350);
+        this.r1 = new Rect(50,50, 100,30, Color.pink, Color.black);
+        this.e1 = new Ellipse(50,100, 100,30, Color.magenta, Color.green);
+        this.t1 = new Triangulo(150, 110, 70, 180, 140, 110, Color.red);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        this.r1.paint(g);
+        this.e1.paint(g);
+        this.t1.paint(g);
+    }
+}
