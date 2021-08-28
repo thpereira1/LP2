410a18d9f699e5ce5bbf85d4317cee6cef797084
Thiago Pereira <86577632+thpereira1@users.noreply.github.com>
2021-08-28 02:45:48 -0300

    Update Triangulo.java

--- 2.Classes/3.1/Figures/Triangulo.java
+++ 2.Classes/3.1/Figures/Triangulo.java
@@ -3,14 +3,16 @@ package Figures;
 import java.awt.*;
 
 public class Triangulo {
-    private int x1,x2,x3;
-	private int y1,y2,y3;
+    private int x1, x2, x3;
+	private int y1, y2, y3;
 	private Color bgc;
 
-    public Triangulo (int x1,int x2,int x3,int y1,int y2,int y3, Color bgc) {
-        this.x1 = x1;
+    public Triangulo (int x1, int x2, int x3, int y1, int y2, int y3, Color bgc) {
+        
+	this.x1 = x1;
 		this.x2 = x2;
 		this.x3 = x3;
+	    
         this.y1 = y1;
 		this.y2 = y2;
 		this.y3 = y3;
