e2761186cf9a3c373923d149458a35ff1c178938
Thiago Pereira <86577632+thpereira1@users.noreply.github.com>
2021-09-18 19:32:38 -0300

    Create ListApp.java

---
+++ 2.Classes/6.1/ListApp.java
@@ -0,0 +1,68 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+import figures.*;
+
+class ListApp {
+    public static void main (String[] args) {
+        ListFrame frame = new ListFrame();
+        frame.setVisible(true);
+    }
+}
+
+class ListFrame extends JFrame {
+    ArrayList<Figure> figs = new ArrayList<Figure>();
+    Random rand = new Random();
+
+    ListFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+
+        this.addKeyListener (
+            new KeyAdapter() {
+                public void keyPressed (KeyEvent evt) {
+                    int x = rand.nextInt(600);
+                    int y = rand.nextInt(600);
+                    int w = rand.nextInt(70);
+                    int h = rand.nextInt(70);
+                    
+                    int x1 = rand.nextInt(600);
+                    int x2 = rand.nextInt(600);
+                    int x3 = rand.nextInt(600);
+                    int y1 = rand.nextInt(600);
+                    int y2 = rand.nextInt(600);
+                    int y3 = rand.nextInt(600);
+                    
+                    Color contorno = new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+                    Color fundo = new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+                    if (evt.getKeyChar() == 'r') {
+                        figs.add(new Rect(x, y, w, h));
+                    } else if (evt.getKeyChar() == 'e') {
+                        figs.add(new Ellipse(x, y, w, h));
+                    } else if (evt.getKeyChar() == 't') {
+                    	figs.add(new Triangulo(x1, x2, x3, y1, y2,y3, fundo));
+                    }
+                    repaint();
+                }
+            }
+        );
+
+        this.setTitle("Lista de Figuras Heterogenea");
+        this.setSize(600, 600);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        for (Figure fig: this.figs) {
+            fig.paint(g);
+        }
+    }
+}
