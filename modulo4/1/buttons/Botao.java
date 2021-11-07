341d6fb6032f9d7174357aa4e999aebe5a2a0e3a
Thiago Pereira <86577632+thpereira1@users.noreply.github.com>
2021-11-07 18:25:51 -0300

    Create Botao.java

---
+++ modulo4/1/buttons/Botao.java
@@ -0,0 +1,41 @@
+package buttons;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import figures.Figure;
+import ivisible.IVisible;
+
+public class Botao implements IVisible {
+	public int idx;
+	private Figure fig;
+	
+	public Botao (int idx, Figure fig) {
+		this.idx = idx;
+		this.fig = fig;
+	}
+	
+	@Override
+	public boolean clicked(int x, int y) {
+		return (fig.x-5<=x && x<=fig.x+fig.w+10 && fig.y-5<=y && y<=fig.y+fig.h+10);
+	}
+
+	@Override
+	public void paint(Graphics g, boolean focado) {
+		Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.black);
+        g2d.drawRect(fig.x-5, fig.y-5, fig.w+10, fig.h+10);
+        
+        if (focado) {
+        	g2d.setColor(Color.gray);
+        } else {
+        	g2d.setColor(Color.LIGHT_GRAY);
+        }
+        g2d.fillRect(fig.x-4, fig.y-4, fig.w+9, fig.h+9);
+        
+        fig.paint(g2d, false);
+		
+	}
+
+}
